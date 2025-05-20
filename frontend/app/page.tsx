23401c8b13419d6c5ab626bdcaffa175245c991e
7johnsz <contatoerro708@gmail.com>
2025-05-19 23:07:12 -0300

    💄 style(home): add hover effect to image
    - add a hover effect with scale transformation to the image for better UX

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="w-full md:w-1/2 flex justify-center">
+          <div className="w-full md:w-1/2 flex justify-center hover:scale-105 transition-transform duration-500">
             <img
               src="/jornals.webp"
               alt="Ilustração de currículo"
@@ -84,41 +84,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-
-
-      {/* <section className="min-h-[80vh] flex items-center justify-center py-16">
-        <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-between px-6 md:px-12 gap-12">
-          <div className="w-full md:w-1/2 text-center md:text-left">
-            <h1 className="text-4xl md:text-6xl font-[900] leading-tight tracking-tighter">
-              Ajudando{" "}
-              <FileUser size={40} className="inline align-middle text-blue-400" />
-              {" "}currículos a{" "}
-              <DoorOpen size={40} className="inline align-middle text-red-400" />
-              {" "}saírem do modo fantasma.
-            </h1>
-
-            <Button asChild className="mt-6 w-full md:w-[250px] py-6 text-lg">
-              <Link href="/docs" className="text-accent font-bold">
-                <Sparkles size={20} className="inline mr-2" />
-                Criar meu currículo
-              </Link>
-            </Button>
-
-            <p className="text-base md:text-lg text-gray-500 mt-6">
-              Ajudando você a encontrar o {" "}
-              <span className="underline font-semibold">seu próximo emprego.</span>
-            </p>
-          </div>
-
-          <div className="w-full md:w-1/2 flex justify-center">
-            <img
-              src="/jornals.webp"
-              alt="Ilustração de currículo"
-              className="w-full  h-auto rounded-lg shadow-lg"
-            />
-          </div>
-        </div>
-      </section> */}
 
       <hr />
 
