f804e6f4902b3720552c28f9186ec14c44e9ef5a
7johnsz <contatoerro708@gmail.com>
2025-05-23 22:37:20 -0300

    🐛 fix(resume): correct resume link in generate page
    - correct the resume link to navigate to the correct dashboard resume page

--- frontend/data/sidebar.ts
+++ frontend/data/sidebar.ts
@@ -22,77 +22,77 @@ export const sidebarData = {
         },
       ],
     },
-    {
-      title: "WALLET",
-      items: [
-        {
-          title: "Portfolio",
-          icon: "CreditCard",
-          href: "/portfolio",
-        },
-        {
-          title: "Transactions",
-          icon: "ArrowRightLeft",
-          href: "/transactions",
-        },
-        {
-          title: "Active Stacking",
-          icon: "Wallet",
-          href: "#",
-          children: [
-            {
-              title: "Ethereum",
-              icon: "CircleDollarSign",
-              href: "/stacking/ethereum",
-              active: true,
-              color: "bg-[#627eea]",
-            },
-            {
-              title: "Bitcoin",
-              icon: "Bitcoin",
-              href: "/stacking/bitcoin",
-              active: true,
-              color: "bg-[#f7931a]",
-            },
-            {
-              title: "Bytecoin",
-              icon: "CircleDollarSign",
-              href: "/stacking/bytecoin",
-              active: true,
-              color: "bg-[#d90cff]",
-            },
-            {
-              title: "Digibyte",
-              icon: "CircleDollarSign",
-              href: "/stacking/digibyte",
-              active: true,
-              color: "bg-[#0033ad]",
-            },
-            {
-              title: "Litecoin",
-              icon: "CircleDollarSign",
-              href: "/stacking/litecoin",
-              active: true,
-              color: "bg-[#345d9d]",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      title: "SETTINGS",
-      items: [
-        {
-          title: "Preferences",
-          icon: "Settings",
-          href: "/preferences",
-        },
-        {
-          title: "API Settings",
-          icon: "Cpu",
-          href: "/api-settings",
-        },
-      ],
-    },
+    // {
+    //   title: "WALLET",
+    //   items: [
+    //     {
+    //       title: "Portfolio",
+    //       icon: "CreditCard",
+    //       href: "/portfolio",
+    //     },
+    //     {
+    //       title: "Transactions",
+    //       icon: "ArrowRightLeft",
+    //       href: "/transactions",
+    //     },
+    //     {
+    //       title: "Active Stacking",
+    //       icon: "Wallet",
+    //       href: "#",
+    //       children: [
+    //         {
+    //           title: "Ethereum",
+    //           icon: "CircleDollarSign",
+    //           href: "/stacking/ethereum",
+    //           active: true,
+    //           color: "bg-[#627eea]",
+    //         },
+    //         {
+    //           title: "Bitcoin",
+    //           icon: "Bitcoin",
+    //           href: "/stacking/bitcoin",
+    //           active: true,
+    //           color: "bg-[#f7931a]",
+    //         },
+    //         {
+    //           title: "Bytecoin",
+    //           icon: "CircleDollarSign",
+    //           href: "/stacking/bytecoin",
+    //           active: true,
+    //           color: "bg-[#d90cff]",
+    //         },
+    //         {
+    //           title: "Digibyte",
+    //           icon: "CircleDollarSign",
+    //           href: "/stacking/digibyte",
+    //           active: true,
+    //           color: "bg-[#0033ad]",
+    //         },
+    //         {
+    //           title: "Litecoin",
+    //           icon: "CircleDollarSign",
+    //           href: "/stacking/litecoin",
+    //           active: true,
+    //           color: "bg-[#345d9d]",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "SETTINGS",
+    //   items: [
+    //     {
+    //       title: "Preferences",
+    //       icon: "Settings",
+    //       href: "/preferences",
+    //     },
+    //     {
+    //       title: "API Settings",
+    //       icon: "Cpu",
+    //       href: "/api-settings",
+    //     },
+    //   ],
+    // },
   ],
 }
