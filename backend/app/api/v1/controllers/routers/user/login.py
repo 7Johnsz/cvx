7a376d5eba911b54c693c399243db9766fbda013
7johnsz <contatoerro708@gmail.com>
2025-06-08 18:34:10 -0300

    ✨ feat(login): implement login with JWT and refresh tokens
    - add JWT token generation for user authentication
    - implement refresh token logic using Redis for storage
    - return user id and name on login

--- backend/app/api/v1/controllers/routers/user/login.py
+++ backend/app/api/v1/controllers/routers/user/login.py
@@ -1,19 +1,34 @@
 from fastapi import Depends, Request
 
 # Config
+from ......config.middleware.config import redis_tokens
 from ......config.middleware.config import limiter
 from ...config.database.models.schema import Users
 from ...config.database.config import get_session
 from ...config.api import router
 
 # Utils
-from ....utils.pass_manager import password_manager
+from ....utils.token_gen import token_manager
 
 # Models
 from ....models.Login import LoginModel 
 
 from sqlmodel import select, Session
 from typing import Union
+
+import datetime
+import jwt
+import os
+
+secret_key = os.getenv("SECRET_KEY")
+
+def create_access_token(user_id: str, secret_key: str):
+    payload = {
+        "sub": user_id,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=15)  
+    }
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+    return token
 
 @router.post("/login")
 @limiter.limit("30/minute")
@@ -26,15 +41,31 @@ async def login(
         if login_data.password and login_data.email:
             existing_user = session.exec(
                 select(Users).where(
-                    Users.email == login_data.email
+                    Users.email == login_data.email,
                 )
             ).first()
             
-            if existing_user and password_manager.verify_password(login_data.password, existing_user.password):
+            if existing_user and token_manager.decode(login_data.password, existing_user.password):
+                access_token = create_access_token(secret_key=str(secret_key), user_id=str(existing_user.id))
+                refresh_token = token_manager.encode(str(existing_user.id))
+                
+                teste = await redis_tokens.set(
+                    refresh_token,
+                    existing_user.id, 
+                    ex=60*60*24*7 # Store token for 7 days  
+                )
+                
+                print(f"Token stored in Redis: {teste}")
+                
                 return {
                     "message": "Login successful",
+                    "refresh_token": refresh_token,
+                    "access_token": access_token,
+                    "user": {
+                        "id": existing_user.id,
+                        "name": existing_user.name,
+                    }
                 }
-            
             return {"error": "Invalid email or password"}
         
         return {"error": "Invalid user data"}
